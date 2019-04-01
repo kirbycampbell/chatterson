e512e9d18493b88d2a9900bf09662939f607811c
kirbycampbell <jkirbycampbell@gmail.com>
2019-03-31 20:34:24 -0700

    Update TypeBox.js

--- src/Components/TypeBox.js
+++ src/Components/TypeBox.js
@@ -15,37 +15,37 @@ export const TypeBox = props => {
 
   // Send assigns Msg state and sends it to DB....
   const send = async () => {
-    const postDeets = {
-      body: "Stinker",
-      createdAt: ""
-    };
-    const message = await API.graphql(
-      graphqlOperation(mutations.createPost, { input: postDeets })
-    );
-    // Resets Message form to empty...
-    setMessage("");
-    console.log("Show New Post::: ");
-    console.log(message);
-
+    // Creates Convo Post
     const newConvoPost = await API.graphql(
       graphqlOperation(mutations.createConvoPosts, {
         input: {
-          convoPostsContentsId: message.data.createPost.id,
+          //convoPostsConversationsId: message1.data.createPost.id
           convoPostsConversationsId: props.convo
         }
       })
     );
     console.log("Convo Post::: ");
     console.log(newConvoPost);
-    // await API.graphql(
-    //   graphqlOperation(mutations.createConvoPosts, {
-    //     input: {
-    //       convoPostsContentsId: message.data.createPost.id,
-    //       convoPostsConversationsId: props.selectedConvo
-    //     }
-    //   })
-    // );
+    // Creates the New Post
+    const message1 = await API.graphql(
+      graphqlOperation(mutations.createPost, {
+        input: {
+          body: `${message}`,
+          createdAt: "",
+          postConversationId: newConvoPost.data.createConvoPosts.id
+        }
+      })
+    );
+    // Resets Message form to empty...
+    setMessage("");
+    console.log("Show New Post::: ");
+    console.log(message1);
   };
+
+  // TOMORROW:
+  // Try Adjusting the GraphQL schema even further.
+  // Maybe a join table isn't needed for posts...
+  // A Post only needs one Convo, a Convo needs many Posts.
 
   const showConvo = async () => {
     const convoo = await API.graphql(
